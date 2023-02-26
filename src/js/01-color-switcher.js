7d0b8d7a7265d0bad206ce370e3ff7297c29f0bb
Ruslan Garmash <garmash.rb@gmail.com>
2023-02-26 14:25:52 +0200

    add 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let intervalId = null;
+
+startButton.addEventListener('click', function () {
+  startButton.disabled = true; // делаем кнопку неактивной
+  intervalId = setInterval(function () {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopButton.addEventListener('click', function () {
+  clearInterval(intervalId);
+  startButton.disabled = false; // возвращаем кнопку в активное состояние
+});
